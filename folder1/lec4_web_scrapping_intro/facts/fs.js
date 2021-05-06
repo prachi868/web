c5bcf12f4e177b0528b1419e073d499ca5c82cba
prachi <jainprachi595@gmail.com>
2021-05-06 15:34:29 +0530

    cheerio introduced

--- folder1/lec4_web_scrapping_intro/facts/fs.js
+++ folder1/lec4_web_scrapping_intro/facts/fs.js
@@ -1,8 +1,19 @@
-const fs = require("fs");
-//const cheerio = require("cheerio");
+// in pab11 folder
+//npm init -y
+//npm install cheerio
 
+const fs=require("fs");
+const cheerio=require("cheerio");
+let htmlKaData=fs.readFileSync("./index.html","utf8");
+//console.log(htmlKaData); //we have stringified html file
 
-let htmlKaData = fs.readFileSync("./index.html" , "utf8");
-console.log(htmlKaData); // we have stringified html file !!!
+//html file is loaded in cheerio
+let myDocument=cheerio.load(htmlKaData);
+//console.log(myDocument);
 
-// html file is loaded in cheerio
+//document.querySelector("h1");
+let h1Element= myDocument("h1");
+//console.log(h1Element); element->cheerio -> object form mei data
+
+let h1KaDataElement=myDocument("h1").text();
+console.log(h1KaDataElement);
